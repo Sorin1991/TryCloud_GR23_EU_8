a24d1ac269f8ddb3af59c18cfa55530d13b9b3b9
Ulku <ulkucardak@gmail.com>
2022-07-01 20:30:01 +0100

    OnlineStatus feature and step-defs class are created

---
+++ src/test/java/com/trycloud/pages/HomePage.java
@@ -0,0 +1,29 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends LoginBasePage {
+
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//div[@id='expand']")
+    public WebElement avatar;
+
+
+    @FindBy(xpath = "//a[@class='user-status-menu-item__toggle']")
+    public WebElement avatarDropdownStatusOption;
+
+
+    @FindBy(xpath = "//div[@class='modal-container']")
+    public  WebElement onlineStatusPopUpWindow;
+
+
+    @FindBy(xpath = "//button[@class='user-status-menu-item__toggle user-status-menu-item__toggle--inline']")
+    public WebElement onlineStatusField;
+
+}
