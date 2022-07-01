6f4e2afe77001b3ea099f4c33092fca30dd576ec
Ulku <ulkucardak@gmail.com>
2022-07-02 00:29:22 +0100

    Homepage and OnlineStatusFieldPage are updated

--- src/test/java/com/trycloud/pages/OnlineStatusFieldPage.java
+++ src/test/java/com/trycloud/pages/OnlineStatusFieldPage.java
@@ -1,9 +1,15 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class OnlineStatusFieldPage extends HomePage{
+public class OnlineStatusFieldPage {
+
+    public OnlineStatusFieldPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
     @FindBy(xpath = "(//div[@class='user-status-online-select'])[1]")
     public WebElement online;
