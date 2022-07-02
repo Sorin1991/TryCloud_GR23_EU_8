c62e896e901d9ae3a8c7ad81e200dd12f7bf6683
Nikolas Tonic <102160595+NikolasTonic@users.noreply.github.com>
2022-07-02 20:00:11 +0200

    Nikolas_Day1

--- src/test/java/com/trycloud/utilities/Driver.java
+++ src/test/java/com/trycloud/utilities/Driver.java
@@ -13,7 +13,7 @@ public class Driver {
 Creating a private constructor, so we are closing
 access to the object of this class from outside the class
 */
-    public Driver() {
+    private Driver() {
     }
 
     /*
