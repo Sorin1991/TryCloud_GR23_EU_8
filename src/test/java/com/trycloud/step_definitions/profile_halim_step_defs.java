527d2b1f8cf1d5c6bb5bd8897f1bc2f1436a38e6
hiltas73 <h_iltas@yahoo.com>
2022-07-07 10:46:58 +0300

    halim_day1

---
+++ src/test/java/com/trycloud/step_definitions/profile_halim_step_defs.java
@@ -0,0 +1,18 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class profile_halim_step_defs {
+
+    @Given("the user click on the profile avatar")
+    public void the_user_click_on_the_profile_avatar() {
+        // Write code here that turns the phrase above into concrete actions
+        ;
+    }
+    @Then("the user sees the dropdown items")
+    public void the_user_sees_the_dropdown_items() {
+        // Write code here that turns the phrase above into concrete actions
+        ;
+    }
+
+}
