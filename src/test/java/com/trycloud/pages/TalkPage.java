940c525fa379939875f2b2a36cb8d85d8e0bb51b
Ayse Uslusoy <ayseuslusoy38@gmail.com>
2022-07-13 10:57:48 +0100

    I finished my TalkPage

--- src/test/java/com/trycloud/pages/TalkPage.java
+++ src/test/java/com/trycloud/pages/TalkPage.java
@@ -1,5 +1,7 @@
 package com.trycloud.pages;
 
+import com.github.javafaker.Faker;
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -10,13 +12,32 @@ public class TalkPage {
     public TalkPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
-    @FindBy(xpath="(//a[@href='http://qa.trycloud.net/index.php/apps/spreed/'][1])")
+    @FindBy(xpath="(//li[@data-id='spreed'])[1]")
    public WebElement talkButton;
 
-    @FindBy(xpath="(//input[@type='text']/..)")
-    public WebElement searchConversationButton;
 
-    @FindBy(xpath="(//div[@class='wrapper'])")
+    @FindBy(css="input[placeholder='Conversation name']")
+    public WebElement talkMentions;
+
+    @FindBy(xpath="//button[@class='navigation__button navigation__button-right primary']")
+    public WebElement addParticipants;
+
+    @FindBy(xpath="//input[@class='set-contacts__input']")
+    public WebElement searchParticipant;
+
+    @FindBy(xpath="//div[@class='wrapper']")
     public WebElement plusButton;
 
+    @FindBy(xpath="//button[@class='navigation__button navigation__button-right primary']")
+    public WebElement createConversation;
+
+
+    @FindBy(xpath="//button[@class='top-bar__button top-bar__button primary']")
+    public WebElement startcall;
+
+
+    public void nameInformation(){
+        Faker faker=new Faker();
+        searchParticipant.sendKeys(faker.name().fullName());
+    }
 }
