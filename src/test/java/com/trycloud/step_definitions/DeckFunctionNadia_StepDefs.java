1d1e0232bad917c707760ea5906f42e6a12e587b
nadianawaz <nadia.nawaz@hotmail.co.uk>
2022-07-15 20:44:12 +0100

    completed Deck function StepDefs

--- src/test/java/com/trycloud/step_definitions/DeckFunctionNadia_StepDefs.java
+++ src/test/java/com/trycloud/step_definitions/DeckFunctionNadia_StepDefs.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class DectFunctionNadia_StepDefs {
+package com.trycloud.step_definitions;
+
+import com.trycloud.pages.DeckFunctionNadiaPage;
+import com.trycloud.pages.LoginBasePage;
+import com.trycloud.pages.NewContactNadia;
+import com.trycloud.utilities.BUtility;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import okhttp3.internal.cache.DiskLruCache;
+import org.openqa.selenium.Keys;
+
+public class DeckFunctionNadia_StepDefs {
+
+    LoginBasePage loginBasePage = new LoginBasePage();
+    DeckFunctionNadiaPage deckFunctionNadiaPage = new DeckFunctionNadiaPage();
+
+
+    @Given("user is on the Deck module")
+    public void userIsOnTheDeckModule() {
+        loginBasePage.loginToDashboardPage();
+        deckFunctionNadiaPage.deckButton.click();
+    }
+
+    @Then("user clicks on the three bars button")
+    public void user_clicks_on_the_three_bars_button() {
+        deckFunctionNadiaPage.threeBarsButton.click();
+    }
+
+    @Then("user clicks on all boards to view all boards")
+    public void userClicksOnAllBoardsToViewAllBoards() {
+        deckFunctionNadiaPage.allBoardsButton.click();
+    }
+
+    @Then("user click on the Add board button")
+    public void userClickOnTheAddBoardButton() {
+        deckFunctionNadiaPage.addBoardButton.click();
+    }
+
+    @And("user can create a new board name {string}")
+    public void userCanCreateANewBoardNameName(String string) {
+        deckFunctionNadiaPage.addTextBoardButton.sendKeys(string);
+        deckFunctionNadiaPage.confirmAddBoardButton.click();
+    }
+
+
+    @Then("user is able to edit an existing board")
+    public void userIsAbleToEditAnExistingBoard() {
+        deckFunctionNadiaPage.selectOptionsBoard.click();
+        deckFunctionNadiaPage.editBoardButton.click();
+        deckFunctionNadiaPage.editEditBoardButton.sendKeys(" editTEST");
+        deckFunctionNadiaPage.confirmEditBoardButton.click();
+
+
+    }
+
+    @And("the user is able to delete an existing board")
+    public void theUserIsAbleToDeleteAnExistingBoard() {
+        deckFunctionNadiaPage.selectOptionsBoard.click();
+        deckFunctionNadiaPage.deleteBoardButton.click();
+        deckFunctionNadiaPage.confirmDeleteButton.click();
+
+
+    }
 }
