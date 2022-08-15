6a4b88c45a42c976c2a26d1d35ad03db16c5e4b4
mohziaei <mohziaeii@gmail.com>
2022-08-15 18:19:30 +0430

    I created step defs without my implementation

---
+++ src/test/java/com/trycloud/step_definitions/CalendarDisplay_Step_Definitions.java
@@ -0,0 +1,20 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class CalendarDisplay_Step_Definitions {
+
+
+    @Given("User Clicks on calendar button")
+    public void user_clicks_on_calendar_button() {
+
+
+    }
+    @Then("User should be able to see Calendar page")
+    public void user_should_be_able_to_see_calendar_page() {
+
+    }
+
+
+}
